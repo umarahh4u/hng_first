aae474f1abbbf1f2e1f5f852ef47356cbb352da1
umarahh4u <umarahh4u@gmail.com>
2025-01-29 00:11:30 +0100

    inprove the readability of the code

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 
 // body parser
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json({ limit: "5kb" }));
+app.use(express.urlencoded({ extended: true, limit: "5kb" }));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
@@ -18,10 +18,8 @@ app.listen(port, () => {
 });
 
 // server.listen
-
-
 app.get("/api/v1/user", (req, res) => {
-const email = req.query.email ? req.query.email : "email@gmail.com" ; 
+const email = req.query.email || "email@gmail.com" ; 
 const date = new Date().toISOString();
 
   res.status(200).json({
