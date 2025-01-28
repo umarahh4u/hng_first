d36ff04cd0066792a1dcd8943eeee36cb102e125
umarahh4u <umarahh4u@gmail.com>
2025-01-28 23:13:30 +0100

    initial
    commencement of the first HNG project

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors")
+
+require("dotenv").config();
+
+const middlewares = require("./middleware");
+
+const app = express();
+app.use(cors());
+
+// body parser
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App runnung on port ${port}`);
+});
+
+// server.listen
+
+
+app.get("/api/v1/user", (req, res) => {
+const {email} = req.body;
+const date = new Date().toISOString();
+
+  res.status(200).json({
+        "email": email,
+        "current_datetime": date,
+        "github_url": "<https://github.com/yourusername/your-repo>"
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
