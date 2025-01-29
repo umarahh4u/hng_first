6fd4bd7736128345b7c55d25261e19fb09cc84ab
umarahh4u <umarahh4u@gmail.com>
2025-01-29 06:42:42 +0100

    update date fotmat by remove microsecond

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
 // server.listen
 app.get("/api/v1/user", (req, res) => {
 const email = req.query.email || "email@gmail.com" ; 
-const date = new Date().toISOString();
+const date = new Date().toISOString().split('.')[0] + 'Z';
 
   res.status(200).json({
         "email": email,
