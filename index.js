a0a9468b336235eb874bb90fcef429152d103de4
umarahh4u <umarahh4u@gmail.com>
2025-01-29 07:10:23 +0100

    update

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
-
+app.listen(port, () => {
+  console.log(`App runnung on port ${port}`);
+});
 
 // server.listen
 app.get("/api/v1/user", (req, res) => {
@@ -25,10 +27,6 @@ const date = new Date().toISOString().split('.')[0] + 'Z';
         "github_url": "https://github.com/umarahh4u/hng_first"
   });
 });
-
-app.listen(port, () => {
-    console.log(`App runnung on port ${port}`);
-  });
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
